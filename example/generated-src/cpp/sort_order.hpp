45ac32bbb96487006e5181e97c4faee8a6047365
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-08 16:18:38 +0100

    rebase

--- example/generated-src/cpp/sort_order.hpp
+++ example/generated-src/cpp/sort_order.hpp
@@ -12,6 +12,8 @@ enum class sort_order : int {
     DESCENDING,
     RANDOM,
 };
+std::string to_string(const sort_order& sort_order);
+std::ostream &operator<<(std::ostream &os, const sort_order &o);
 
 }  // namespace textsort
 
